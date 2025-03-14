b0d01064fea8bf148a57d2ce5906c6fa19d5edb2
SABARI <sabarisekar777@gmail.com>
2025-03-14 15:29:04 +0530

    Fixed Bug

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -2,6 +2,5 @@ import logo from "./images/logo.png";
 import profileImg from "./images/profile.jpg";
 import fitnessApp from "./images/fitnessApp.png";
 import blog from "./images/blog.png";
-import noorShop from "./images/noorShop.png";
 
-export { logo, profileImg, fitnessApp, blog, noorShop };
+export { logo, profileImg, fitnessApp, blog };
